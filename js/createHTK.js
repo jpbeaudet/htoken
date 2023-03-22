5508b7f749743a1f1a7397d504e03c2c31313bcc
Jean-Philippe Beaudet <jpbeaudet@users.noreply.github.com>
2023-03-22 13:05:07 -0400

    Update createHTK.js

--- js/createHTK.js
+++ js/createHTK.js
@@ -1,30 +1,71 @@
-// create htk
-$("#create-htk-form").submit(async (event) => {
+const createHTKForm = document.querySelector("#create-htk-form");
+
+createHTKForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const htkName = $("#htk-name").val();
-  const htkSymbol = $("#htk-symbol").val();
-
-  // check if input values are valid
-  if (!htkName || !htkSymbol) {
-    alert("Please enter valid input values");
+  // Check if user is connected to wallet
+  if (!window.ethereum || !window.ethereum.selectedAddress) {
+    alert("Please connect to a wallet to create HToken.");
     return;
   }
 
-  // call createHTK function from smart contract
-  const receipt = await window.hTokenInstance.methods
-    .createHTK(htkName, htkSymbol)
-    .send({ from: window.selectedAccount });
+  // Get form data
+  const name = createHTKForm.elements.namedItem("htk-name").value;
+  const symbol = createHTKForm.elements.namedItem("htk-symbol").value;
 
-  // wait for transaction to be confirmed
-  const txHash = receipt.transactionHash;
-  let txConfirmed = false;
-  while (!txConfirmed) {
-    const receipt = await window.web3.eth.getTransactionReceipt(txHash);
-    if (receipt && receipt.status) {
-      txConfirmed = true;
-      alert("HToken created successfully!");
-    }
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // wait 1 second before checking again
+  // Check if form data is valid
+  if (!name || !symbol) {
+    alert("Please fill in all form fields to create HToken.");
+    return;
+  }
+
+  try {
+    // Get contract instance
+    const web3 = new Web3(window.ethereum);
+    const accounts = await web3.eth.getAccounts();
+    const htkContract = new web3.eth.Contract(HTK_ABI);
+
+    // Estimate gas cost
+    const gasPrice = await web3.eth.getGasPrice();
+    const gasLimit = 300000;
+    const data = htkContract.deploy({
+      data: HTK_BYTECODE,
+      arguments: [name, symbol],
+    }).encodeABI();
+    const nonce = await web3.eth.getTransactionCount(accounts[0], "latest");
+    const tx = {
+      from: accounts[0],
+      to: null,
+      gasPrice: web3.utils.toHex(gasPrice),
+      gasLimit: web3.utils.toHex(gasLimit),
+      nonce: web3.utils.toHex(nonce),
+      data: data,
+    };
+    const estimatedGas = await web3.eth.estimateGas(tx);
+
+    // Send transaction to create HToken
+    const transactionHash = await htkContract
+      .deploy({
+        data: HTK_BYTECODE,
+        arguments: [name, symbol],
+      })
+      .send(
+        {
+          from: accounts[0],
+          gasPrice: web3.utils.toHex(gasPrice),
+          gasLimit: web3.utils.toHex(estimatedGas),
+        },
+        async (error, transactionHash) => {
+          if (error) {
+            alert(`Error creating HToken: ${error}`);
+          } else {
+            alert(
+              `HToken created successfully. Transaction hash: ${transactionHash}`
+            );
+          }
+        }
+      );
+  } catch (error) {
+    alert(`Error creating HToken: ${error}`);
   }
 });
