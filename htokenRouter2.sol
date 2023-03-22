a97e915610fdb6827deaa68dd7230b3971967ce7
Jean-Philippe Beaudet <jpbeaudet@users.noreply.github.com>
2023-03-22 11:41:11 -0400

    Update htokenRouter2.sol

--- htokenRouter2.sol
+++ htokenRouter2.sol
@@ -7,46 +7,8 @@ import "./HTokenFactory.sol";
 contract HTokenRouter {
     HTokenFactory public factory;
 
-    // Registers an HToken contract for exchange.
-    function registerHToken(address hTokenAddress) public {
-        // Ensure that the caller is the owner of the HToken contract.
-        require(factory.hTokenName(hTokenAddress) != "", "HToken does not exist in factory");
-        require(factory.hTokenSymbol(hTokenAddress) != "", "HToken does not exist in factory");
-
-        // Create an instance of the HToken contract.
-        HToken hToken = HToken(hTokenAddress);
-
-        // Ensure that the caller is the owner of the HToken contract.
-        require(hToken.owner() == msg.sender, "Only the owner of the HToken contract can register it for exchange");
-
-        // Register the HToken contract for exchange.
-        factory.registerHToken(hToken);
-    }
-
-    // Unregisters an HToken contract from the exchange.
-    function unregisterHToken(address hTokenAddress) public {
-        // Ensure that the caller is the owner of the HToken contract.
-        require(factory.hTokenName(hTokenAddress) != "", "HToken does not exist in factory");
-        require(factory.hTokenSymbol(hTokenAddress) != "", "HToken does not exist in factory");
-
-        // Create an instance of the HToken contract.
-        HToken hToken = HToken(hTokenAddress);
-
-        // Ensure that the caller is the owner of the HToken contract.
-        require(hToken.owner() == msg.sender, "Only the owner of the HToken contract can unregister it from the exchange");
-
-        // Unregister the HToken contract from the exchange.
-        factory.unregisterHToken(hTokenAddress);
-    }
-
     // Exchanges one HToken for another.
     function swapExactHTKForHTK(address fromHTK, uint256 fromAmount, address toHTK) public {
-        // Ensure that both HToken contracts are registered for exchange.
-        require(factory.hTokenName(fromHTK) != "", "From HToken is not registered for exchange");
-        require(factory.hTokenSymbol(fromHTK) != "", "From HToken is not registered for exchange");
-        require(factory.hTokenName(toHTK) != "", "To HToken is not registered for exchange");
-        require(factory.hTokenSymbol(toHTK) != "", "To HToken is not registered for exchange");
-
         // Get the instances of the HToken contracts.
         HToken fromToken = HToken(fromHTK);
         HToken toToken = HToken(toHTK);
