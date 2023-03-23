ce7497912cadc15b0e5b00becfb7a52a7c1fa5da
Jean-Philippe Beaudet <jpbeaudet@users.noreply.github.com>
2023-03-23 12:24:16 -0400

    Create blockExplorer.js

---
+++ js/blockExplorer.js
@@ -0,0 +1,70 @@
+// Import necessary libraries
+const Web3 = require('web3');
+const HTokenABI = require('./HTokenABI.json');
+
+// Initialize Web3 with Infura endpoint
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/your-project-id'));
+
+// Address of the HToken smart contract
+const htkAddress = '0x...';
+
+// Instantiate HToken contract object with ABI and address
+const htkContract = new web3.eth.Contract(HTokenABI, htkAddress);
+
+// Get total supply of HTK tokens
+htkContract.methods.totalSupply().call()
+  .then(totalSupply => {
+    // Populate the total supply element in the HTML block explorer
+    const totalSupplyElement = document.getElementById('htk-total-supply');
+    totalSupplyElement.innerHTML = totalSupply;
+  })
+  .catch(error => {
+    console.error('Failed to get total supply:', error);
+  });
+
+// Get balance of a specific account
+const accountAddress = '0x...';
+htkContract.methods.balanceOf(accountAddress).call()
+  .then(balance => {
+    // Populate the balance element in the HTML block explorer
+    const balanceElement = document.getElementById('htk-balance');
+    balanceElement.innerHTML = balance;
+  })
+  .catch(error => {
+    console.error('Failed to get balance:', error);
+  });
+
+// Get the last 10 transactions for the HToken contract
+web3.eth.getPastLogs({
+  address: htkAddress,
+  fromBlock: 0,
+  toBlock: 'latest',
+  topics: [null, null, null, null] // This is the event signature for the Transfer event
+})
+  .then(logs => {
+    // Process the logs to get the transaction data
+    const transactions = logs.map(log => {
+      const [from, to, value] = web3.eth.abi.decodeParameters(['address', 'address', 'uint256'], log.data);
+      return {
+        from,
+        to,
+        value
+      };
+    }).slice(0, 10); // Only show the last 10 transactions
+
+    // Populate the transaction table in the HTML block explorer
+    const transactionsTable = document.getElementById('htk-transactions');
+    transactionsTable.innerHTML = ''; // Clear any existing rows
+    transactions.forEach(transaction => {
+      const row = transactionsTable.insertRow(-1);
+      const fromCell = row.insertCell(0);
+      const toCell = row.insertCell(1);
+      const valueCell = row.insertCell(2);
+      fromCell.innerHTML = transaction.from;
+      toCell.innerHTML = transaction.to;
+      valueCell.innerHTML = transaction.value;
+    });
+  })
+  .catch(error => {
+    console.error('Failed to get past logs:', error);
+  });
